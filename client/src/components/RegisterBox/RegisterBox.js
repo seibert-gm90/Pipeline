6204eb4c7492cc6e88a867134fb25cd7844ae3f9
Grisha Kramer <gregorykramer@HAL-8998.local>
2020-04-07 20:23:27 -0400

    Register

---
+++ client/src/components/RegisterBox/RegisterBox.js
@@ -0,0 +1,38 @@
+class RegisterBox extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+
+    submitRegister(e) {}
+    render() {
+        return (
+            <div className="inner-container">
+                <div className="header">Register</div>
+                <div Classname="box">
+
+                    <div className="input-group">
+                        <label htmlFor="username">Username</label>
+                        <input
+                            type="text"
+                            name="username"
+                            className="login-input"
+                            placeholder="Username"/>
+                    </div>
+
+                    <div className="input-group">
+                        <label htmlFor="email">Email</label>
+                        <input
+                         type="text"
+                         name="email"
+                         className="login-input"
+                         placeholder="Password"/>
+                    </div>
+                    <button
+                    type="button"
+                    className="login-btn"
+                    onClick={this.submitRegister.bind(this)}>Register</button>
+                </div>
+            </div>
+        )
+    }
