0654078f72278c2cc7614976c37391dea3c42b2e
Gabby Jones <jones.gm90@gmail.com>
2020-04-09 19:36:25 -0400

    commiting models

--- models/assistant.js
+++ models/assistant.js
@@ -5,7 +5,6 @@ const assistantSchema = new Schema({
   name: { type: String, required: true },
   phoneNumber: {type: String, required:true},
   email: {type: String, required:true},
-  rating: Number,
   date: { type: Date, default: Date.now }
 });
 
