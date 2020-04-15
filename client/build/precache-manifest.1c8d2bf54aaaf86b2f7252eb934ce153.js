fd535530b69e92e326af37197970ae69e9410319
Grisha Kramer <gregorykramer@HAL-8998.local>
2020-04-14 21:01:51 -0400

    changing login file structure

---
+++ client/build/precache-manifest.1c8d2bf54aaaf86b2f7252eb934ce153.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f499aabc42abb3d78167",
+    "url": "/static/css/main.53f64884.chunk.css"
+  },
+  {
+    "revision": "f499aabc42abb3d78167",
+    "url": "/static/js/main.8de02ba6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "688e062f7528c44b801d",
+    "url": "/static/js/2.fdf311d3.chunk.js"
+  },
+  {
+    "revision": "49f4f5ce83fccb0b487c3239c76c9ea9",
+    "url": "/index.html"
+  }
+];
