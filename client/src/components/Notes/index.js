6a6c2d13ecaf9f0d9a340ac7d0b2216dfa5a4c36
Rapunzel <Laurenminton19@yahoo.com>
2020-04-24 19:01:19 -0400

    testing

--- client/src/components/Notes/index.js
+++ client/src/components/Notes/index.js
@@ -1,21 +1,16 @@
-import React from "react"; 
-import "./notes.css";
+// import React from "react"; 
+// import "./notes.css";
 
+// function StickyNotes() {
+//   $("#create").click(function() {
+//     $(this).before("<textarea></textarea>");
+//   });
+//   return (
+//     <textarea>This is a sticky note.  Leave yourself a message.</textarea>
+//     <div id="create">+</div> 
+//   );
+// }
 
-
-function StickyNotes() {
-  $("#create").click(function() {
-    $(this).before("<textarea></textarea>");
-  });
-  return (
-    <textarea>This is a sticky note.  Leave yourself a message.</textarea>
-    <div id="create">+</div> 
-  );
-}
-
-
-
-
-export default StickyNotes;
+// export default StickyNotes;
 
 
