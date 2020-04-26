490d866b3dbfaba07edaf47742287e7e81cc073b
Grisha Kramer <gregorykramer@HAL-8998.local>
2020-04-25 20:43:10 -0400

    Notes and Dashboard links in hamburger work now

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -12,17 +12,17 @@ function Header() {
           <span></span>
 
           <ul id="menu">
-          <a href="#">
+          <a href="/home">
               <li>Dashboard</li>
             </a>
-            <a href="#">
-              <li>Calendar</li>
+            <a href="/Calendar">
+              <li>Calendar (coming soon!)</li>
             </a>
-            <a href="#">
+            <a href="/notes">
               <li>Notes</li>
             </a>
             <a href="#">
-              <li>Chat (Coming Soon)</li>
+              <li>Chat (Coming soon!)</li>
             </a>
             <a href="#">
               <li>Log Out</li>
