d32990038fd4c1c30216c258bce8ad5c572cc91e
Gabby Jones <jones.gm90@gmail.com>
2020-04-30 09:26:43 -0400

    adding seed info for case collection

---
+++ scripts/caseSeed.js
@@ -0,0 +1,27 @@
+  const caseSeed = [
+  {
+  name: "Roland Jones",
+  phoneNumber: "434-547-8907",
+  email: "housecall@gmail.com",
+  broker:"", 
+  property:"1906 Grove Ave",
+  date: { type: Date, default: Date.now }
+  }, 
+  {
+  name: "Grisha Kramer",
+  phoneNumber: "434-847-6890",
+  email: "kramer@gmail.com",
+  broker:"", 
+  property:"1900 Cary Ave",
+  date: { type: Date, default: Date.now }
+  }, 
+  {
+  name: "Lauren Minton",
+  phoneNumber: "434-512-2487",
+  email: "minton@gmail.com",
+  broker:"", 
+  property:"4576 Grove Ave",
+  date: { type: Date, default: Date.now }
+  }, 
+
+  ]
