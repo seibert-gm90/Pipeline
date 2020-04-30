8007c93747d2a4669d58135f119c006037067dd9
Rapunzel <Laurenminton19@yahoo.com>
2020-04-29 23:02:24 -0400

    added + to dashboard, fixed footer list items

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -12,16 +12,16 @@ function Footer() {
               <a href="mailto:thisisfake@gmail.com">Contact Us</a>
             </li>
             <li className="list">
+              <a href="">About Pipeline</a>
+            </li>
+            {/* <li className="list">
+              <a href="">Privacy &amp; Security</a>
+            </li> */}
+            <li className="list">
               <a href="">Careers</a>
             </li>
             <li className="list">
-              <a href="">Privacy &amp; Security</a>
-            </li>
-            <li className="list">
-              <a href="">About Pipeline</a>
-            </li>
-            <li className="list">
-              <a href="">Feedback</a>
+              <a href="">Our Developers</a>
             </li>
           </ul>
         </p>
@@ -35,7 +35,7 @@ function Footer() {
             <li className="list">Richmond, VA</li>
             <li className="list">23173</li>
             <li className="list google">
-              <a href="https://goo.gl/maps/mWEHKqH5Q6s">View on Google Maps</a>
+              <a href="https://www.google.com/maps/place/410+Westhampton+Way,+Richmond,+VA+23173/@37.5751669,-77.5429033,17z/data=!4m13!1m7!3m6!1s0x89b114a18c3fffff:0x1da45f9a0c6f393b!2s410+Westhampton+Way,+Richmond,+VA+23173!3b1!8m2!3d37.5751669!4d-77.5407146!3m4!1s0x89b114a18c3fffff:0x1da45f9a0c6f393b!8m2!3d37.5751669!4d-77.5407146">View on Google Maps</a>
             </li>
           </ul>
         </p>
@@ -44,7 +44,7 @@ function Footer() {
         <p>
           <span>Legal</span>
           <ul>
-            <li className="list">Copyright &copy;2012 - 2016</li>
+            <li className="list">Copyright &copy;2020</li>
             <li className="list">All rights reserved.</li>
             <li className="list">Terms &amp; Conditions</li>
           </ul>
